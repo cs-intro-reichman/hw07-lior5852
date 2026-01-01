6ad161a54f83e03d98ec6c195017bc6d499e398e
lior5852 <lior.tzur@post.runi.ac.il>
2026-01-01 19:32:22 +0200

    check

--- Koch.java
+++ Koch.java
@@ -29,7 +29,22 @@ public class Koch {
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
 		StdDraw.line(x1, y1, x2, y2);
 		//// Write the rest of your code below.
+		if ( n==0)
+			StdDraw.line(x2, y2, x1, y1);
+		return;
 	}
+	double xa = x1 + (x2 - x1)/3.0;
+	double ya = y1 + (y2 - y1)/3.0;
+
+	double xb = x1 + 2*(x2-x1)/3.0;
+	double yb = y1 + 2*(y2-y1)/3.0;
+
+
+	double xc = (Math.sqrt(3)/6)* (y1 - y2) + 0.5 * (x1 + x2);
+	double xc = (Math.sqrt(3)/6)* (x1 - x2) + 0.5 * (y1 + y2);
+
+
+
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
 	public static void snowFlake(int n) {
@@ -38,5 +53,9 @@ public class Koch {
 		StdDraw.setXscale(0, 1.1);
 		// Draws a Koch snowflake of depth n
 		//// Write the rest of your code below.
+		double x1=0.2, y1=0.8;
+		double x2=0.2, y2=0.8;
+		double x3=0.2, y3=0.8;
+		
 	}
 }
